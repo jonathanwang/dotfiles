6ee651b4035332463b6deb4562b9e3f141285c9f
Jonathan Wang <jonathanwang>
2016-01-16 22:49:05 -0500

    install script removes old dotfiles directory first

--- install.sh
+++ install.sh
@@ -13,23 +13,25 @@ files="vimrc vim bash-git-prompt"    # list of files/folders to symlink in homed
 
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
+if [ -e $olddir ]; then
+    rm -rf $olddir
+fi
 mkdir -p $olddir
-echo "...done"
 
 # change to the dotfiles directory
 echo "Changing to the $dir directory"
 cd $dir
-echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+echo "Moving any existing dotfiles from ~ to $olddir"
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
 
 # Git lg
+echo "Adding 'git lg'"
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
 # Clone Vundle
