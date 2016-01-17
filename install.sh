824b810486eaaf9ecea32573510b9cd4b9d93a80
Jonathan Wang <jonathanwang>
2016-01-16 23:38:22 -0500

    install custom profile

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 ############################
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+# Dotfiles install script
 ############################
 
-########## Variables ########## 
+########## Variables ##########
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc vim bash-git-prompt"    # list of files/folders to symlink in homedir
+dotfiles=(vimrc vim)   	 # list of files/folders to symlink in homedir
+bash_files=(.profile .bash_profile .bashrc)  # bash files to add the command to source the 'myprofile'
+custom_profile=myprofile
 
 ###############################
 
@@ -22,9 +24,9 @@ mkdir -p $olddir
 echo "Changing to the $dir directory"
 cd $dir
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 echo "Moving any existing dotfiles from ~ to $olddir"
-for file in $files; do
+for file in ${dotfiles[@]}; do
     mv ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
@@ -36,10 +38,23 @@ git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Crese
 
 # Clone Vundle
 if [ -e $dir/vim/bundle/Vundle.vim ]; then
-    rm -rf $dir/vim/bundle/Vundle.vim 
+    rm -rf $dir/vim/bundle/Vundle.vim
 fi
 git clone https://github.com/gmarik/Vundle.vim.git $dir/vim/bundle/Vundle.vim
 
 # Install Vundle and plugins
 vim +PluginInstall +qall
+
+# Add the command to source the shell script to '.profile', '.bash_profile', and/or '.bashrc'
+# if it has not already been added (DOTFILES), and if the file exists.
+if [ -z "$DOTFILES" ]; then
+    for file in ${bash_files[@]}; do
+		echo "Adding sourcing custom profile to $file"
+        if [ -e ~/$file ]; then
+			echo "# Sourcing custom bash profile" >> ~/$file
+			echo "source $dir/$custom_profile" >> ~/$file
+			echo "" >> ~/$file
+		fi
+	done
+fi
 
